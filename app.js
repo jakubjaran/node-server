d1227d175131dce886d697f8621096e04ca8b2f8
jakubjaran <jakubjaran@gmail.com>
2021-06-12 23:41:31 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(3000);
