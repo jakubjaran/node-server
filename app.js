f9357d43524a3b35cb59b06eee66c27b1ce32499
jakubjaran <jakubjaran@gmail.com>
2021-06-12 23:48:12 +0200

    Use routes files

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 const express = require('express');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.get('/', (req, res, next) => {
-  res.send('<h1>Hello World!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
