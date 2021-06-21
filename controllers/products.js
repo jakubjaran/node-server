400d99b19fd45734f7af6abb514c646ac59c973e
jakubjaran <jakubjaran@gmail.com>
2021-06-21 10:50:07 +0200

    Using product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,17 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', { docTitle: 'MyShop - Add Product' });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', {
     prods: products,
     docTitle: 'MyShop',
