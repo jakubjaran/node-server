2183df974c82f5551e31d42a2da35fe8c60d117b
jakubjaran <jakubjaran@gmail.com>
2021-07-01 11:22:59 +0200

    Displaying products in cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -30,8 +30,22 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    docTitle: 'MyShop - Your Cart',
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        docTitle: 'MyShop - Your Cart',
+        products: cartProducts,
+      });
+    });
   });
 };
 
