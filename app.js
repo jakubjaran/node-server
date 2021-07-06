ce1a0db5ce5593f01bb715461c26f59577cfd528
jakubjaran <jakubjaran@gmail.com>
2021-07-06 11:30:48 +0200

    Connect to mongodb

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+const mongoConnect = require('./utils/database');
 
 const app = express();
 
@@ -13,11 +15,14 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 const errorController = require('./controllers/error');
 
 app.use(errorController.getPage404);
 
-app.listen(3000);
+mongoConnect(client => {
+  console.log(client);
+  app.listen(3000);
+});
--- utils/database.js
+++ utils/database.js
@@ -1,1 +1,15 @@
-// mongodb+srv://node:node-express1@cluster0.hqcee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const { MongoClient } = require('mongodb');
+
+const uri =
+  'mongodb+srv://node:node-express1@cluster0.hqcee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const connectMongo = callback => {
+  MongoClient.connect(uri)
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => console.log(err));
+};
+
+module.exports = connectMongo;
