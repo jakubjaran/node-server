1a572079dc125f1e8ac86180e908242a513a62bd
jakubjaran <jakubjaran@gmail.com>
2021-07-11 08:31:25 +0200

    Adding relational order data

--- models/user.js
+++ models/user.js
@@ -74,9 +74,18 @@ class User {
 
   createOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            username: this.username,
+            email: this.email,
+          },
+        };
+        return db.collection('orders').insertOne(order);
+      })
       .then(result => {
         this.cart = { items: [] };
         return db
