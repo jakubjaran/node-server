1f2e5cc977e60d2dfde3e32e63453b2a7d8beb94
jakubjaran <jakubjaran@gmail.com>
2021-07-14 10:30:28 +0200

    Displaying orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,8 +76,7 @@ exports.postDeleteCartItem = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         docTitle: 'MyShop - Your Orders',
