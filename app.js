6a65505c68d97b011ea489bc57e938b5574e3e6f
jakubjaran <jakubjaran@gmail.com>
2021-07-16 23:36:36 +0200

    Storing sessions in mongoDB

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://node:node-express1@cluster0.hqcee.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -22,6 +30,7 @@ app.use(
     secret: 'my secret string',
     resave: false,
     saveUninitialized: false,
+    store: store,
   })
 );
 
@@ -42,11 +51,8 @@ const errorController = require('./controllers/error');
 
 app.use(errorController.getPage404);
 
-const dbUri =
-  'mongodb+srv://node:node-express1@cluster0.hqcee.mongodb.net/shop?retryWrites=true&w=majority';
-
 mongoose
-  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
