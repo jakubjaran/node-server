00faa0a11e616a033973c599fd44d5b459a96490
jakubjaran <jakubjaran@gmail.com>
2021-07-18 22:59:50 +0200

    Deleting session on logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,3 +19,12 @@ exports.postLogin = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/');
+  });
+};
