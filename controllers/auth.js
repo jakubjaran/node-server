c414e6664675fe1413dbd1e49b9f874a42d01c0b
jakubjaran <jakubjaran@gmail.com>
2021-07-18 23:23:58 +0200

    Hide add to cart on details page, redirect after login session saved

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,9 +13,10 @@ exports.postLogin = (req, res, next) => {
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-    })
-    .then(() => {
-      res.redirect('/');
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
     })
     .catch(err => console.log(err));
 };
